b1f53ac9389bf4ae11640f4b3b62883723eba30b
punjordi <jspun@ualberta.ca>
2019-08-08 16:07:19 -0600

    almost

--- src/components/SummonerName.js
+++ src/components/SummonerName.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react'
 
 export default class SummonerName extends Component {
 
-    fetchName = () => {
-        let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + this.props.name + "?api_key=RGAPI-ca1f4d68-d7e4-4dcf-a5d4-1389ec5eaf6f"
-        console.log(url)
-        fetch(url)
-        .then(function(response){
-            return response.json
-        })
-        .then(function(myJson) {
-            console.log(JSON.stringify(myJson));
-            return JSON.stringify(myJson)
-          });
+    state = {
+        summonerID:''
+
+    };
+
+    componentDidMount(){
+        fetch("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + this.props.name + "?api_key=RGAPI-ca1f4d68-d7e4-4dcf-a5d4-1389ec5eaf6f",
+        {mode :'no-cors',
+        "Access-Control-Allow-Origin" : "*", 
+        "Access-Control-Allow-Credentials" : true} )
+        .then(results => {
+
+            return results.json();
+            ``
+  
+
+            })
+            
+
     }
     
     render() {
         
         return(
             <div>
-                {this.fetchName()}
+                
             </div>
             
         )
