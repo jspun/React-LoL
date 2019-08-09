78e22e4619cd457ee4df83fc18a0bfd9871a09bc
punjordi <jspun@ualberta.ca>
2019-08-09 11:59:17 -0600

    successfully pulled object from League API

--- src/components/SummonerName.js
+++ src/components/SummonerName.js
@@ -3,26 +3,26 @@ import React, { Component } from 'react'
 export default class SummonerName extends Component {
 
     state = {
-        summonerID:''
-
+        summonerID:'',
+        profileID:''
     };
 
     componentDidMount(){
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + this.props.name + "?api_key=RGAPI-ca1f4d68-d7e4-4dcf-a5d4-1389ec5eaf6f";
+        const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + this.props.name + "?api_key=RGAPI-224d5e49-ea7d-4413-96b6-40efe7d0c637";
 
         fetch(proxyurl + url)
-        .then(response => response.text())
-        .then(contents => console.log(contents))
+        .then(response => response.json())
+        .then(contents => this.setState({summonerID: contents.accountId, profileID: contents.profileIconId}))
         .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
-            
+        
 
     }
     
     render() {
-        
         return(
             <div>
+                <p></p>
                 
             </div>
             
