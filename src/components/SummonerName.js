8b4b24f19c22a8d8b65b92db46cc70e1c9e61aa1
punjordi <jspun@ualberta.ca>
2019-08-11 18:34:27 -0600

    test

--- src/components/SummonerName.js
+++ src/components/SummonerName.js
@@ -23,24 +23,25 @@ export default class SummonerName extends Component {
         const apikey = "RGAPI-1434f867-9ded-44c8-a153-b6b9cf227970";
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-        const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + this.props.name + "?api_key=" + apikey;
-
+        const Summonerurl = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + this.props.name + "?api_key=" + apikey;
+        //const mathurl = 
 
 
         fetch(proxyurl + url)
         .then(response => response.json())
         .then(contents => this.setState({summonerID: contents.accountId, profileID: contents.profileIconId, summonerLevel: contents.summonerLevel, name: contents.name}))
+        .then(test => console.log(test))
         .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
-        
+
 
     }
     
     render() {
-        let profileIconUrl = "http://ddragon.leagueoflegends.com/cdn/9.3.1/img/profileicon/" + this.state.profileID + ".png"
+        let profileIconUrl = "https://ddragon.leagueoflegends.com/cdn/9.3.1/img/profileicon/" + this.state.profileID + ".png"
         return(
             <div className = "container">
                 <img src = {profileIconUrl} alt = "SummonerIcon" width = "200px" height = "auto" style={{padding: "40px"}}/>
-                <div className = "SummonerPhoto" style = {this.getStyle()}>
+                <div className = "SummonerInfo" style = {this.getStyle()}>
                     
                     {this.state.name}
                 </div> 
